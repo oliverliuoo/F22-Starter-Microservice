773b25b6e71a7b39e450ebb857b77a44ed5a1fea
Oliver Liu <oliverliu@Olivers-MacBook-Pro-2.local>
2022-09-18 14:56:30 -0400

    Add prod configuration.

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 """
 Config file to store global variables.
 """
-DB_USER = 'root'
+DB_USER = 'admin'
 DB_PASSWORD = 'meimima.'
-DB_HOST = 'localhost'
+DB_HOST = 'database-1.cjfpr994lzwv.us-east-2.rds.amazonaws.com'
